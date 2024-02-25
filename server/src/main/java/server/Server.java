a1eddb60b48633e95810ab24db479c568ae9f179
dshuld <dms267@byu.edu>
2024-02-24 22:43:19 -0700

    get test web page working

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -4,12 +4,22 @@ import spark.*;
 
 public class Server {
 
+    public static void main(String[] args) {
+        var port = 8080;
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        Server server = new Server();
+        server.run(port);
+    }
+
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.get("/test",(req, res) -> "Hello BYU!");
 
         Spark.awaitInitialization();
         return Spark.port();
