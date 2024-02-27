239be5eef35c2a223af29279d2df72a215544719
dshuld <dms267@byu.edu>
2024-02-27 10:58:04 -0700

    remove shared function

--- server/src/main/java/handler/Handler.java
+++ server/src/main/java/handler/Handler.java
@@ -1,13 +1,10 @@
 package handler;
 
+import dataAccess.AuthDao;
+import dataAccess.MemoryAuthDao;
 import spark.Request;
 import spark.Response;
 
 public interface Handler {
     public abstract Object handle(Request req, Response res);
-
-    private boolean isValidAuthtoken(String auth) {
-        //validate authtoken
-        return false;
-    }
 }
