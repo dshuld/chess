d03b03ff44beee5c178fc5c05c9b334b03b4124a
dshuld <dms267@byu.edu>
2024-02-27 10:57:50 -0700

    add method

--- server/src/main/java/dataAccess/AuthDao.java
+++ server/src/main/java/dataAccess/AuthDao.java
@@ -5,6 +5,7 @@ import model.AuthData;
 public interface AuthDao {
     public void createAuth(AuthData data);
     public AuthData getAuth(AuthData data);
+    public AuthData getUser(AuthData data);
     public void deleteAuth(AuthData data);
     public void clear();
 }
