56d79d1308d7180bbb717348c2dae4f11a170955
dshuld <dms267@byu.edu>
2024-03-04 18:41:14 -0700

    implement SQL authDAO methods

--- server/src/main/java/dataAccess/sql/SQLAuthDao.java
+++ server/src/main/java/dataAccess/sql/SQLAuthDao.java
@@ -1,10 +1,10 @@
 package dataAccess.sql;
 
 import dataAccess.DataAccessException;
-import dataAccess.interfaces.AuthDao;
-import dataAccess.interfaces.UserDao;
+import dataAccess.interfaces.*;
 import model.AuthData;
 
+import java.sql.ResultSet;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -37,21 +37,78 @@ public class SQLAuthDao extends ConnectionManager implements AuthDao {
 
     @Override
     public void createAuth(AuthData data) {
-
+        String sql = "INSERT INTO auth (authtoken, username) VALUES (?, ?)";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(sql)) {
+                preparedStatement.setString(1,data.authtoken());
+                preparedStatement.setString(2,data.username());
+                if (preparedStatement.executeUpdate() >= 1) {
+                    System.out.println("Added authtoken for user: "+data.username());
+                }
+                else {
+                    System.out.println("Failed to add authtoken for user: "+data.username());
+                }
+            }
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
     }
 
     @Override
     public AuthData getUser(AuthData data) {
-        return null;
+        String sql = "SELECT * FROM auth WHERE authtoken=?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(sql)) {
+                preparedStatement.setString(1,data.authtoken());
+
+                ResultSet rs = preparedStatement.executeQuery();
+                if (rs.next()) {
+                    String authtoken = rs.getString("authtoken");
+                    String username = rs.getString("username");
+                    return new AuthData(authtoken, username);
+                }
+                else {
+                    return null;
+                }
+            }
+        } catch(Exception e) {
+            System.out.println(e.toString());
+            return null;
+        }
     }
 
     @Override
     public void deleteAuth(AuthData data) {
-
+        String sql = "DELETE FROM auth WHERE authtoken=?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(sql)) {
+                preparedStatement.setString(1,data.authtoken());
+                if (preparedStatement.executeUpdate() >= 1) {
+                    System.out.println("Deleted authtoken for user: "+data.username());
+                }
+                else {
+                    System.out.println("Failed to delete authtoken for user: "+data.username());
+                }
+            }
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
     }
 
     @Override
     public void clear() {
-
+        String sql = "DELETE FROM auth";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(sql)) {
+                if (preparedStatement.executeUpdate() >= 1) {
+                    System.out.println("Cleared authtoken table");
+                }
+                else {
+                    System.out.println("Failed to clear authtoken table");
+                }
+            }
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
